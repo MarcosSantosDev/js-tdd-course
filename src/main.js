6e8c0a9f4e5c545fb2b3835392cdc83ec399be58
Marcos Santos <marcosadriano034@gmail.com>
2021-05-13 11:21:56 -0300

    chore: migrate modules to ES6

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-module.exports = {
-  sum: (num1, num2) => num1 + num2,
-  sub: (num1, num2) => num1 - num2,
-  mult: (num1, num2) => num1 * num2,
-  div: (num1, num2) => {
-    if (num2 === 0) {
-      return 'It is not possible to divide by zero!';
-    }
-    return num1 / num2;
-  },
+const sum = (num1, num2) => num1 + num2;
+const sub = (num1, num2) => num1 - num2;
+const mult = (num1, num2) => num1 * num2;
+const div = (num1, num2) => {
+  if (num2 === 0) {
+    return 'It is not possible to divide by zero!';
+  }
+  return num1 / num2;
+};
+
+export {
+  sum,
+  sub,
+  mult,
+  div,
 };
